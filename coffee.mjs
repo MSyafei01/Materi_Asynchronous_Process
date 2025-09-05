e8c9abe2bb9ca21438a36e915f5c695b0ebad1b2
MSyafei <mochamadsyafei2701@gmail.com>
2025-09-05 22:11:08 +0700

    up

--- coffee.mjs
+++ coffee.mjs
@@ -1,9 +1,37 @@
-function promiseExecutor(resolve, reject) {
-  setTimeout(() => {
-    resolve('You did it!');
-  }, 2000);
-    }
+const API = {
+  fetch(delay, simulateError = false) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (simulateError) {
+          return reject(new Error(`Error from delay ${delay}`));
+        }
 
-export function doSomething() {
-  return new Promise(promiseExecutor);
-}
+        return resolve(`Data from delay ${delay}`);
+      }, delay);
+    });
+  }
+};
+
+const sampleErrorData = [
+  {
+    delay: 100,
+    simulateError: false,
+  },
+  {
+    delay: 50,
+    simulateError: true,
+  },
+];
+
+const sampleSuccessData = [
+  {
+    delay: 100,
+    simulateError: false,
+  },
+  {
+    delay: 50,
+    simulateError: false,
+  },
+];
+
+export { API, sampleErrorData, sampleSuccessData };
--- main.mjs
+++ main.mjs
@@ -1,22 +1,23 @@
-import { doSomething } from './utils.mjs';
+import { API, sampleErrorData, sampleSuccessData } from './coffee.mjs';
 
-async function promiseWithAsyncAwait() {
-  try {
-    console.log('Start.');
-
-    const result = await doSomething();
-    console.log(result);
-
-    console.log('End.');
-  } catch (error) {
-    console.log(error.message);
-  }
+/**
+ * Fungsi processData:
+ * 1. Mengembalikan data dari pemanggilan API.fetch berdasarkan argumen `data` yang diberikan.
+ * 2. Membangkitkan error jika API.fetch mengembalikan Promise reject.
+ */
+function processData(data) {
+  return Promise.all(
+    data.map(item => API.fetch(item.delay, item.simulateError))
+  );
 }
 
-promiseWithAsyncAwait();
+// Test case
+processData(sampleErrorData)
+  .then(console.log)
+  .catch(console.log); 
+// → Throw exception: Error from delay 50
 
-/* Output:
-Start.
-You did it.
-End.
-*/
+processData(sampleSuccessData)
+  .then(console.log)
+  .catch(console.log); 
+// → expected output: ['Data from delay 100', 'Data from delay 50']
